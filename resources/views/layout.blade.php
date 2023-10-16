a6916562b235b70a8856ed46fb4a2a36542280d0
SaoriGoto <birdcutebird@gmail.com>
2023-10-17 01:48:20 +0900

    feat(views): created layout file for blade templates

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>食物繊維摂取量診断@yield('title')</title>
+
+    </head>
+    <body>
+@yield('contents')
+    </body>
+</html>
