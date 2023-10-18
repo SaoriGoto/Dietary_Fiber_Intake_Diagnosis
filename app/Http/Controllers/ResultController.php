e1e2665e91bf27b83ecf06b60f295223ee9fc713
SaoriGoto <birdcutebird@gmail.com>
2023-10-19 00:27:11 +0900

    feat(controllers): add calculation process for successfully validated data
    - Added logic to divide user-input values by 100 for calculation, considering the database values are per 100g.
    - Define the target values.

---
+++ app/Http/Controllers/ResultController.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\DietaryFiberData;
+
+class ResultController extends Controller
+{
+    //calculate and display results
+    public function result(FoodstuffRegisterPostRequest $request)
+    {
+        //Retrieve data that has successfully passed validation
+        $foodstuff_name = $request->input('foodstuff_name');
+        $quantity = $request->input('quantity');
+
+        //Retrieve the necessary data from the database
+        $foodstuffData = DietaryFiberData::where('foodstuff_name', $foodstuff_name)->firstOrFail();
+
+        //Calculate the actual intake based on user input quantity
+        $solubleFiberPer100g = $foodstuffData->soluble_fiber;
+        $insolubleFiberPer100g = $foodstuffData->insoluble_fiber;
+
+        $solubleFiber = ($solubleFiberPer100g / 100) * $quantity;
+        $insolubleFiber = ($insolubleFiberPer100g / 100) * $quantity;
+
+        //Define the target values
+        $targetValueSoluble = 13;
+        $targetValueInsoluble = 6.5;
+
+        return view('result', compact(
+            'solubleFiber',
+            'insolubleFiber',
+            'targetValueSoluble',
+            'targetValueInsoluble'
+        ));
+    }
+}
