a4a26197262441a04d08b22187cf8b70d05623b3
SaoriGoto <birdcutebird@gmail.com>
2023-10-19 23:48:07 +0900

    feat(controllers):add logic to enable data retrieval in InsolubleFiberListModel

--- app/Http/Controllers/DietaryFiberListController.php
+++ app/Http/Controllers/DietaryFiberListController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\SolubleFiberList;
+use App\Models\InsolubleFiberList;
+
 
 class DietaryFiberListController extends Controller
 {
@@ -11,6 +13,8 @@ class DietaryFiberListController extends Controller
     public function list()
     {
         $soluble_fiber_list = SolubleFiberList::all();
-        return view('dietary_fiber_list', compact('soluble_fiber_list'));
+        $insoluble_fiber_list = InsolubleFiberList::all();
+
+        return view('dietary_fiber_list', compact('soluble_fiber_list', 'insoluble_fiber_list'));
     }
 }
