f9ae150c44839af12b49d9ad068801f3e3558230
SaoriGoto <birdcutebird@gmail.com>
2023-10-19 23:40:00 +0900

    feat(views): add mechanism to retrieve and display data from soluble_fiber_lists_table and insoluble_fiber_lists_table
    - Added a mechanism display data from the 'soluble_fiber_lists_table' and 'insoluble_fiber_lists_table' using their respective models.

---
+++ resources/views/dietary_fiber_list.blade.php
@@ -0,0 +1,45 @@
+@extends('layout')
+
+{{-- Main contents --}}
+@section('contents')
+    <h1>食物繊維が多い食材</h1>
+    <h2>水溶性食物繊維が多い食材</h2>
+        <table border="1">
+            <thead>
+            <tr>
+                <th>食材名
+                <th>食物繊維量(可食部100gあたり)
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($soluble_fiber_list as $row)
+                <tr>
+                    <td>{{ $row->foodstuff_name }}
+                    <td>{{ $row->soluble_fiber }}
+                </tr>
+            @endforeach
+        </tbody>
+        </table>
+
+    <h2>不溶性食物繊維が多い食材</h2>
+        <table border="1">
+        <thead>
+            <tr>
+                <th>食材名
+                <th>食物繊維量(可食部100gあたり)
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($insoluble_fiber_list as $row)
+                <tr>
+                    <td>{{ $row->foodstuff_name }}
+                    <td>{{ $row->insoluble_fiber }}
+                </tr>
+            @endforeach
+        </tbody>
+        </table>
+
+    <a href="/foodstuff_register">食材登録画面に戻る</a><br>
+    <hr>
+    <a href="/top">トップページに戻る</a>
+@endsection
