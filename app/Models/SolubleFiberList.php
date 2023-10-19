650100082d7fcfd2b1e4da27580f1ca89a29992d
SaoriGoto <birdcutebird@gmail.com>
2023-10-19 18:47:09 +0900

    refactor(database): rename dietary_fiber_lists_table and corresponding model to SolubleFiberList

--- app/Models/SolubleFiberList.php
+++ app/Models/SolubleFiberList.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DietaryFiberList extends Model
+class SolubleFiberList extends Model
 {
     use HasFactory;
 
-    protected $table = 'dietary_fiber_lists';
+    protected $table = 'soluble_fiber_lists';
     protected $primaryKey = 'id';
 }
