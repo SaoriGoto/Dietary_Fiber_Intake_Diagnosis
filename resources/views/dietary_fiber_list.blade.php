0afe0232b6804e3336ed9cf26d6a5ad6530ea013
SaoriGoto <birdcutebird@gmail.com>
2023-10-20 23:01:47 +0900

    feat(views): wrap each table in a div container and use Flexbox for horizontal alignment
    - Wrapped each table in a div container and utilized Flexbox layout to arrange tables in a horizontal layout within the view.

--- resources/views/dietary_fiber_list.blade.php
+++ resources/views/dietary_fiber_list.blade.php
@@ -3,41 +3,47 @@
 {{-- Main contents --}}
 @section('contents')
     <h1>食物繊維が多い食材</h1>
-    <h2>水溶性食物繊維が多い食材</h2>
-        <table border="1">
-            <thead>
-            <tr>
-                <th>食材名
-                <th>食物繊維量(可食部100gあたり)
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($soluble_fiber_list as $row)
-                <tr>
-                    <td>{{ $row->foodstuff_name }}
-                    <td>{{ $row->soluble_fiber }}
-                </tr>
-            @endforeach
-        </tbody>
-        </table>
+    <div style="display: flex; flex-direction: row;">
+        <div style="margin-right: 20px;">
+            <h2>水溶性食物繊維が多い食材</h2>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>食材名
+                        <th>食物繊維量(可食部100gあたり)
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($soluble_fiber_list as $row)
+                        <tr>
+                            <td>{{ $row->foodstuff_name }}
+                            <td>{{ $row->soluble_fiber }}
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
-    <h2>不溶性食物繊維が多い食材</h2>
-        <table border="1">
-        <thead>
-            <tr>
-                <th>食材名
-                <th>食物繊維量(可食部100gあたり)
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($insoluble_fiber_list as $row)
-                <tr>
-                    <td>{{ $row->foodstuff_name }}
-                    <td>{{ $row->insoluble_fiber }}
-                </tr>
-            @endforeach
-        </tbody>
-        </table>
+        <div>
+            <h2>不溶性食物繊維が多い食材</h2>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>食材名
+                        <th>食物繊維量(可食部100gあたり)
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($insoluble_fiber_list as $row)
+                        <tr>
+                            <td>{{ $row->foodstuff_name }}
+                            <td>{{ $row->insoluble_fiber }}
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <a href="/foodstuff_register">食材登録画面に戻る</a><br>
     <hr>
