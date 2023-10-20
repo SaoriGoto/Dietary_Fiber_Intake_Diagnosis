09742ee0010f5c1f9ae9f5b2233423959d8d6442
SaoriGoto <birdcutebird@gmail.com>
2023-10-21 00:42:35 +0900

    feat(views): add foodstuff quantity guidelines table to the foodstuff-register page
    - Removed the link for '/foodstuff-quantity-guidelines' and added a table with guidelines directly on the 'foodstuff-register' page for user convenience.

--- resources/views/foodstuff_register.blade.php
+++ resources/views/foodstuff_register.blade.php
@@ -2,22 +2,58 @@
 
 {{--Main contents--}}
 @section('contents')
-        <h1>食材登録</h1>
-        @if($errors->any())
-            <div>
-            @foreach($errors->all() as $error)
-                {{$error}}<br>
-            @endforeach
-            </div>
-        @endif
-        <p>あなたが食べた食材を登録してください。</p>
-            <form action="/foodstuff-register" method="post">
-                @csrf
-                食材名：<input name="foodstuff_name" value="{{old('foodstuff_name')}}" placeholder="全角カタカナで入力" required><br>
-                量　　：<input name="quantity" value="{{old('quantity')}}" placeholder="半角数字で入力" required>g<br>
-            <a href="/foodstuff-quantity-guidelines">※食材量の目安はこちら</a><br>
+    <h1>食材登録</h1>
+    @if($errors->any())
+        <div>
+        @foreach($errors->all() as $error)
+            {{$error}}<br>
+        @endforeach
+        </div>
+    @endif
+    <p>あなたが食べた食材を登録してください。</p>
+        <form action="/foodstuff-register" method="post">
+            @csrf
+            食材名：<input name="foodstuff_name" value="{{old('foodstuff_name')}}" placeholder="全角カタカナで入力" required><br>
+            量　　：<input name="quantity" value="{{old('quantity')}}" placeholder="半角数字で入力" required>g<br>
+
+            <h2>食材量の目安</h2>
+                <table border="1">
+                    <tr>
+                        <th>食材名
+                        <th>目安
+                    </tr>
+
+                    <tr>
+                        <td>ショクパン
+                        <td>60g(6枚切り1枚)
+                    </tr>
+                    <tr>
+                        <td>ウドン
+                        <td>200g(1玉あたり)
+                    </tr>
+                    <tr>
+                        <td>サツマイモ
+                        <td>250g(1本あたり)
+                    </tr>
+                    <tr>
+                        <td>キャベツ
+                        <td>300g(1/4玉あたり)
+                    </tr>
+                    <tr>
+                        <td>ホウレンソウ
+                        <td>200g(1束あたり)
+                    </tr>
+                    <tr>
+                        <td>リンゴ
+                        <td>300g(1個あたり)
+                    </tr>
+                    <tr>
+                        <td>シイタケ
+                        <td>15g(1個あたり)
+                    </tr>
+                </table>
             <button>結果を表示する</button>
-            </form>
-        <hr>
-        <a href="/top">トップページに戻る</a>
+        </form>
+    <hr>
+    <a href="/top">トップページに戻る</a>
 @endsection
