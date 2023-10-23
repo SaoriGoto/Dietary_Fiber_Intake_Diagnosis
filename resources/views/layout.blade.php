d023e05918bd36198c3459199e72e6d8a6c21e29
SaoriGoto <birdcutebird@gmail.com>
2023-10-24 04:27:27 +0900

    feat(views): added link to load CSS styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>食物繊維摂取量診断@yield('title')</title>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
     <body>
 @yield('contents')
