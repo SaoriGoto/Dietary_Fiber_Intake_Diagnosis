52eb7ca77408507141db729b2989dbf566761ead
SaoriGoto <birdcutebird@gmail.com>
2023-10-24 04:39:43 +0900

    feat(views): separated header and main into classes for CSS styling
    - Separated the 'header' and 'main' elements into classes to apply CSS styling.
    - Added an image and title at the top of the page.
    - Included the English version in the title.

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,15 +2,25 @@
 
 {{--Main contents--}}
 @section('contents')
-<h1>食物繊維摂取量診断</h1>
-<p>あなたが食べた食材からどれくらいの食物繊維が摂取できているか診断するサービスです。</p>
 
-<h3>食物繊維とは</h3>
-<p>食物繊維は小腸で消化・吸収されずに、大腸まで達する食品成分です。<br>
-  摂取することにより整腸効果、血糖値上昇の抑制、血液中のコレステロール濃度の低下などの効果も期待されます。</p>
+<header>
+    <img src="{{ asset('images/salad.jpg') }}" alt="salad" class="top-image">
+    <div class="top-logo-1">食物繊維摂取量診断</div>
+    <div class="top-logo-2">Dietary fiber intake diagnosis</div>
 
-<form action="/foodstuff-register" method="get">
-    <button>診断する</button>
-</form>
+</header>
 
+<main>
+    <div class="main-contents">
+        <p>あなたが食べた食材からどれくらいの食物繊維が摂取できているか診断するサービスです。</p>
+
+        <h3>食物繊維とは</h3>
+        <p>食物繊維は小腸で消化・吸収されずに、大腸まで達する食品成分です。<br>
+            摂取することにより整腸効果、血糖値上昇の抑制、血液中のコレステロール濃度の低下などの効果も期待されます。</p>
+
+        <form action="/foodstuff-register" method="get">
+        <button>診断する</button>
+        </form>
+    </div>
+</main>
 @endsection
