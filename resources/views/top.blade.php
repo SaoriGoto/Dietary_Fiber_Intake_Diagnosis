582954c14a16486f4a9bfaa7116f31082bebd8f8
SaoriGoto <birdcutebird@gmail.com>
2023-10-24 22:38:40 +0900

    feat(views): added <h1> tag inside  in <header><div class='top-logo-1'>, centered content in <main>, and button styling
    - Added <h1> tag inside  in <header><div class='top-logo-1'>.
    - Implemented CSS styles in <main> to center content.
    - Increased the size of the <button class='diagnosis-button'> and added hover effects to change its color on cursor hover.

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,21 +5,21 @@
 
 <header>
     <img src="{{ asset('images/salad.jpg') }}" alt="salad" class="top-image">
-    <div class="top-logo-1">食物繊維摂取量診断</div>
+    <div class="top-logo-1"><h1>食物繊維摂取量診断</h1></div>
     <div class="top-logo-2">Dietary fiber intake diagnosis</div>
-
 </header>
 
 <main>
-    <div class="main-contents">
+    <div class="main-contents-1">
         <p>あなたが食べた食材からどれくらいの食物繊維が摂取できているか診断するサービスです。</p>
-
-        <h3>食物繊維とは</h3>
+    </div>
+    <div class="main-contents-2">
+        <h3>ー食物繊維とはー</h3>
         <p>食物繊維は小腸で消化・吸収されずに、大腸まで達する食品成分です。<br>
             摂取することにより整腸効果、血糖値上昇の抑制、血液中のコレステロール濃度の低下などの効果も期待されます。</p>
 
         <form action="/foodstuff-register" method="get">
-        <button>診断する</button>
+        <button class="diagnosis-button">診断する</button>
         </form>
     </div>
 </main>
