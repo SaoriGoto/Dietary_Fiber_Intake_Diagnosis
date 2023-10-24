9d59d023c46da2092793e5d8586b6f25b57bdb95
SaoriGoto <birdcutebird@gmail.com>
2023-10-25 03:57:11 +0900

    refactor(views): changed  <header> and <main> for the top page's class name.
    - Changed  <header> to 'top-header' class for the top page.
    - Changed <main> to 'top-main' class for the top page.
    - Because separate the <header> for the top page from the others.

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,17 +3,17 @@
 {{--Main contents--}}
 @section('contents')
 
-<header>
-    <img src="{{ asset('images/salad.jpg') }}" alt="salad" class="top-image">
-    <div class="top-logo-1"><h1>食物繊維摂取量診断</h1></div>
-    <div class="top-logo-2">Dietary fiber intake diagnosis</div>
+<header class="top-header">
+    <img src="{{ asset('images/top-header.jpg') }}" alt="top-header" class="top-header-image">
+    <div class="top-header-logo-1"><h1>食物繊維摂取量診断</h1></div>
+    <div class="top-header-logo-2">Dietary fiber intake diagnosis</div>
 </header>
 
-<main>
-    <div class="main-contents-1">
+<main class="top-main">
+    <div class="top-main-contents-1">
         <p>あなたが食べた食材からどれくらいの食物繊維が摂取できているか診断するサービスです。</p>
     </div>
-    <div class="main-contents-2">
+    <div class="top-main-contents-2">
         <h3>ー食物繊維とはー</h3>
         <p>食物繊維は小腸で消化・吸収されずに、大腸まで達する食品成分です。<br>
             摂取することにより整腸効果、血糖値上昇の抑制、血液中のコレステロール濃度の低下などの効果も期待されます。</p>
